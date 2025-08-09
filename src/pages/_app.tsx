af25bb4679e5b8578884cc3241d5ee139169a864
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-08-09 16:46:35 +0530

    context configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,30 @@
 import "@/styles/globals.css";
 import '@radix-ui/themes/styles.css'
-
 import { Theme } from '@radix-ui/themes'
 import type { AppProps } from "next/app";
+import { UserDataProvider } from "@/contexts/user-data-context";
+import { AuthProvider, useAuth } from "@/contexts/auth-context";
+
+import { ReactNode } from "react";
+
+function AppProviders({ children }: { children: ReactNode }) {
+  const { user } = useAuth();
+  
+  return (
+    <UserDataProvider user={user}>
+      {children}
+    </UserDataProvider>
+  );
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Theme appearance="light" accentColor="blue" grayColor="gray">
-      <Component {...pageProps} />
-    </Theme>
+    <AuthProvider>
+      <AppProviders>
+        <Theme appearance="light" accentColor="blue" grayColor="gray">
+          <Component {...pageProps} />
+        </Theme>
+      </AppProviders>
+    </AuthProvider>
   )
 }
